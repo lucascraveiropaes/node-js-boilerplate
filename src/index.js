cd4a61c40df1bcac693208f6b533422558da790e
Lucas <lucascraveiropaes@gmail.com>
2022-05-27 14:28:53 -0300

    Example with express

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import "dotenv/config";
+import bodyParser from "body-parser";
+import express from "express";
+import cors from "cors";
 
-console.log(process.env.ENVIROMENT);
+const app = express();
+
+app.use( cors() );
+app.use( bodyParser.urlencoded({ extended: false }) );
+app.use( bodyParser.json() );
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Listening: http://localhost:" + process.env.PORT)
+});
